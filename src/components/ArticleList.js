38f44bc0bbeeeb4b29559fa06dcfd5cef2f68da7
annaVih <anna-kobzii@mail.ru>
2018-12-29 19:00:21 +0000

    Open selected article on wikipedia on click

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,14 +4,15 @@ import ReactHtmlParser from 'react-html-parser';
 
 const ArticleList = ({query}) => {
     const articles = useResources(query)
-    console.log(articles.length)
-
 
     return (
             <ul>
                 { 
                     articles.map(article => (
-                        <li key={article.pageid}>
+                        <li
+                            key={article.pageid} 
+                            onClick={() => window.open(`https://en.wikipedia.org/?curid=${article.pageid}`, '_blank')}
+                        >
                             <h3>{article.title}</h3>
                             <p>{ReactHtmlParser(article.snippet)}</p>
                         </li>
