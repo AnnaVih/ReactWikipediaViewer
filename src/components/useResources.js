493fb17479a593b1f571ccfa877f92b5262c97e3
annaVih <anna-kobzii@mail.ru>
2018-12-29 18:45:27 +0000

    Add cleanup on unmount component

--- src/components/useResources.js
+++ src/components/useResources.js
@@ -1,16 +1,23 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+let isMounted;
 const useResources = resource => {
     const [resources, setResources] = useState([])
     
     const fetchResource = async resource =>  {
         const response = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${resource}`)
-        setResources(response.data.query.search)
+        if(isMounted){
+            setResources(response.data.query.search)
+        }
     }
 
     useEffect(() => {
+        isMounted = true
         fetchResource(resource)
+        return () => {
+            isMounted = false
+        }
     }, [resource])
 
     return resources
