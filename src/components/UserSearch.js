62fedb3b3abbc02f1af7bd99637e996fa3b482ac
annaVih <anna-kobzii@mail.ru>
2018-12-29 12:50:12 +0000

    First commit

---
+++ src/components/UserSearch.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const UserSearch = props => {
+    return <React.Fragment>
+        <input type="text" placeholder="Search"/>
+        <button type="button">Search</button>
+    </React.Fragment>
+}
+
+export default UserSearch
---
+++ src/components/api.js
@@ -0,0 +1,7 @@
+//get list of data by search query
+//`https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${search}`
+//open page by id
+//https://en.wikipedia.org/?curid=21555729
+
+//random page
+//https://en.wikipedia.org/wiki/Special:Random.
