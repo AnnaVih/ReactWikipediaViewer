f49efa689b86fc4dac82bc260dc03abfb2510d12
annaVih <anna-kobzii@mail.ru>
2018-12-29 19:19:19 +0000

    Updates

---
+++ precache-manifest.8e3829fcf4ada22fb481b92415bea70d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71b4aad3b80256a9b56a05726effe162",
+    "url": "/ReactWikipediaViewer/static/media/wikipediaLogo.71b4aad3.png"
+  },
+  {
+    "revision": "0fe55cf12d9080d7a5d0",
+    "url": "/ReactWikipediaViewer/static/js/runtime~main.0fe55cf1.js"
+  },
+  {
+    "revision": "7cb8860b1df2c4b4edaf",
+    "url": "/ReactWikipediaViewer/static/js/main.7cb8860b.chunk.js"
+  },
+  {
+    "revision": "40caccc89281ebe11b3a",
+    "url": "/ReactWikipediaViewer/static/js/1.40caccc8.chunk.js"
+  },
+  {
+    "revision": "7cb8860b1df2c4b4edaf",
+    "url": "/ReactWikipediaViewer/static/css/main.5d13a963.chunk.css"
+  },
+  {
+    "revision": "ef3a2b67d0df77f48113660235aad631",
+    "url": "/ReactWikipediaViewer/index.html"
+  }
+];
